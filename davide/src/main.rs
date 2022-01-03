d37c6e5a2a6f40c17cc682193b73338a2396144b
Matteo Pizzoli <matteo.pizzoli89@gmail.com>
2022-01-03 16:43:00 +0100

    add audio crate to implement in notification

--- davide/src/main.rs
+++ davide/src/main.rs
@@ -1,7 +1,17 @@
 use notify_rust::Notification;
+use rusty_audio::Audio;
+
 fn main() {
     Notification::new().summary("Firefox Vaffanculi")
         .body("Ciao ciao ciao")
         .icon("firefox")
         .show().unwrap();
+    
+    let mut audio = Audio::new();
+    audio.add("davide", "audio.mp3");
+    audio.play("davide");
+    audio.wait();
+
 }
+
+
