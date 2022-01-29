e925aa3237262c3ed67e558fe6b04a07f480740b
Matteo Pizzoli <matteo.pizzoli89@gmail.com>
2022-01-29 16:19:51 +0100

    use normal txt file

--- davide/src/main.rs
+++ davide/src/main.rs
@@ -1,17 +1,65 @@
 use notify_rust::Notification;
 use rusty_audio::Audio;
+use std::{
+    fs::File,
+    io::{prelude::*, BufReader},
+    path::Path,
+};
+use rand::seq::SliceRandom;
+
 
 fn main() {
-    Notification::new().summary("Firefox Vaffanculi")
-        .body("Ciao ciao ciao")
+    
+
+    let jokes = robba("../davide.txt");
+    let joke: Vec<_> = jokes
+        .choose_multiple(&mut rand::thread_rng(), 1)
+        .collect();
+//    println!("{:?}", joke[0]);
+
+//    for line in jokes {
+//        println!("ciao {:?}", line);
+//        
+//    }
+
+
+    Notification::new().summary("A me gli occhi, Davide")
+        .body(joke[0])
         .icon("firefox")
         .show().unwrap();
     
+    let path = Path::new("./src/audio.mp3");
     let mut audio = Audio::new();
-    audio.add("davide", "/home/cerfu/Projects/davide-jokes/davide/src/audio.mp3");
+    audio.add("davide", path.to_str().unwrap() );
     audio.play("davide");
     audio.wait();
 
+
+    //let path = PathBuf::from("./src/audio.mp3");
+    //println!("ciao {:?}", fs::canonicalize(path.to_str().unwrap()));
+//
 }
 
+fn robba(filename: impl AsRef<Path>) -> Vec<String>{
+    let file = File::open(filename).expect("no such file or other problem");
+    let buf = BufReader::new(file);
+    buf.lines()
+        .map(|l| l.expect("Could not parse lines"))
+        .collect()
+}
+
+//    // Read and parse json
+//    let json_file_path = Path::new("../davide.json");
+//    let file = File::open(json_file_path);
+//    println!("ciao {:?}", file);
+//
+//    #[derive(Debug,Deserialize)]
+//    #[serde(transparent)]
+//    struct Jokes {
+//        davide: Vec<String>
+//    
+//    } 
+//    let jokes = serde_json::from_reader(file).expect("error");
+//
+//    Ok(jokes)
 
