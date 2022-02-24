d1d12b069f3f8227f51ba48aa98c39ca2b83b6f4
Matteo Pizzoli <matteo.pizzoli89@gmail.com>
2022-02-24 12:12:46 +0100

    new performance

--- davide/src/main.rs
+++ davide/src/main.rs
@@ -1,46 +1,41 @@
 use notify_rust::Notification;
+use rand::seq::SliceRandom;
 use rusty_audio::Audio;
 use std::{
     fs::File,
     io::{prelude::*, BufReader},
     path::Path,
 };
-use rand::seq::SliceRandom;
-
 
 fn main() {
-    
-
     let jokes = robba("../davide.txt");
-    let joke: Vec<_> = jokes
-        .choose_multiple(&mut rand::thread_rng(), 1)
-        .collect();
-//    println!("{:?}", joke[0]);
+    let joke: Vec<_> = jokes.choose_multiple(&mut rand::thread_rng(), 1).collect();
+    //    println!("{:?}", joke[0]);
 
-//    for line in jokes {
-//        println!("ciao {:?}", line);
-//        
-//    }
+    //    for line in jokes {
+    //        println!("ciao {:?}", line);
+    //
+    //    }
 
-
-    Notification::new().summary("A me gli occhi, Davide")
+    Notification::new()
+        .summary("A me gli occhi, Davide")
         .body(joke[0])
         .icon("firefox")
-        .show().unwrap();
-    
+        .show()
+        .unwrap();
+
     let path = Path::new("./src/audio.mp3");
     let mut audio = Audio::new();
-    audio.add("davide", path.to_str().unwrap() );
+    audio.add("davide", path.to_str().unwrap());
     audio.play("davide");
     audio.wait();
 
-
     //let path = PathBuf::from("./src/audio.mp3");
     //println!("ciao {:?}", fs::canonicalize(path.to_str().unwrap()));
-//
+    //
 }
 
-fn robba(filename: impl AsRef<Path>) -> Vec<String>{
+fn robba(filename: impl AsRef<Path>) -> Vec<String> {
     let file = File::open(filename).expect("no such file or other problem");
     let buf = BufReader::new(file);
     buf.lines()
@@ -57,9 +52,8 @@ fn robba(filename: impl AsRef<Path>) -> Vec<String>{
 //    #[serde(transparent)]
 //    struct Jokes {
 //        davide: Vec<String>
-//    
-//    } 
+//
+//    }
 //    let jokes = serde_json::from_reader(file).expect("error");
 //
 //    Ok(jokes)
-
